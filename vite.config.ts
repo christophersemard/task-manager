a7d896fc27dba1da5c685c94dbc37bf685e7379c
Christopher Semard <christophersemard76@gmail.com>
2025-06-27 14:52:35 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 import lightningcss from "vite-plugin-lightningcss";
 
 export default defineConfig({
-    plugins: [react(), lightningcss()],
+    plugins: [
+        react(),
+        lightningcss({
+            browserslist: [">= 0.25%"],
+            minify: true,
+        }),
+    ],
 });
